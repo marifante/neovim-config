f8ef10f4a330f9af9cfe73a98ce23d3777b696e1
marifante <jnrodriguezz@hotmail.com>
2024-04-10 17:17:09 -0300

    feat: add trailing whitespaces config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,3 +8,22 @@ opt.tabstop = 4 -- A TAB character looks like 4 spaces
 opt.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 opt.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 opt.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+-- Configure to show trailling whitespaces with a middle dot and
+-- highlighter in red
+opt.list = true
+opt.listchars = { tab = "| ", trail = "·" }
+
+vim.cmd([[
+  highlight ExtraWhitespace ctermbg=red guibg=red
+  match ExtraWhitespace /\s\+$/
+  autocmd BufWinEnter * match ExtraWhitespace /\s\+$/
+  autocmd InsertEnter * match ExtraWhitespace /\s\+\%#\@<!$/
+  autocmd InsertLeave * match ExtraWhitespace /\s\+$/
+  autocmd BufWinLeave * call clearmatches()
+]])
+
+-- Command to remove trailling whitespaces from file
+vim.cmd([[
+  command! TrimWhitespace execute ':%s/\s\+$//e' | write
+]])
